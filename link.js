ea66d55ac7bf96d5aa5d7c6cc38e7c89f3ccc196
ishwarya-123 <ishwaryasubramanian7@gmail.com>
2025-02-27 11:38:21 +0530

    msg

---
+++ link.js
@@ -0,0 +1,24 @@
+var grade=50;
+
+if(grade>=90&& grade<=100)
+{
+    console.log("outstanding");
+}
+ else if(grade>=80 && grade<=90)
+    {
+    console.log("excellant");
+
+    }
+
+ else if(grade>=70&& grade<=80)
+   {
+    console.log("average");
+   }
+   else if(grade>=60 && grade<=70)
+   {
+    console.log("normal");
+   }
+  else
+   {
+    console.log("fail");
+   }
